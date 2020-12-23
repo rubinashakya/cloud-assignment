7802a6cba3bc52c88d3388d1b75eb6b89a10ca7e
rubinashakya <rubinashakya68@gmail.com>
2020-12-23 21:30:52 +0545

    add gitignore and form updates

--- js/main.js
+++ js/main.js
@@ -62,4 +62,85 @@ $(document).ready(function() {
     $(".search-btn").click(function(){
         $(this).parent().toggleClass("search-open");
     });
+    
+    $('form[id="contact-form"]').on('submit', function(event){
+        event.preventDefault();
+    })
+
+    $.validator.methods.phone = function( value, element ) {
+        return this.optional( element ) || /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/.test( value );
+    }
+
+
+    $('form[id="contact-form"]').validate({
+        rules: {
+            firstname: 'required',
+            lastname: 'required',
+            emailid: {
+                required: true,
+                email: true,
+            },
+            companyname: {
+                required: true,
+                minlength: 3
+            },
+            jobtitle: 'required',
+            phonenumber: {
+                phone: true
+            }
+        },
+        messages: {
+            firstname: 'This field is required',
+            lastname: 'This field is required',
+            emailid: 'Enter a valid email',
+            companyname: 'This field is reqired',
+            jobtitle: 'This field is required',
+            phonenumber: 'Must be a valid phone number',
+        },
+        
+        errorElement : 'span',
+        errorLabelContainer: '.errorTxt',
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+    // $(function() {
+    // // Initialize form validation on the registration form.
+    // // It has the name attribute "registration"
+    // $(".contact-form").validate({
+    //     // Specify validation rules
+    //     rules: {
+    //     // The key name on the left side is the name attribute
+    //     // of an input field. Validation rules are defined
+    //     // on the right side
+    //     firstname: "required",
+    //     lastname: "required",
+    //     emailid: {
+    //         required: true,
+    //         // Specify that email should be validated
+    //         // by the built-in "email" rule
+    //         email: true
+    //     },
+    //     companyname: {
+    //         required: true,
+    //         minlength: 3
+    //     },
+    //     jobtitle: "required",
+    //     phonenumber: {
+    //         matches: "/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/"
+    //     }
+    //     },
+    //     // Specify validation error messages
+    //     messages: {
+    //     firstname: "Please enter your firstname",
+    //     lastname: "Please enter your lastname",
+    //     email: "Please enter a valid email address"
+    //     },
+    //     // Make sure the form is submitted to the destination defined
+    //     // in the "action" attribute of the form when valid
+    //     submitHandler: function(form) {
+    //     form.submit();
+    //     }
+    // });
+    // });
 });
